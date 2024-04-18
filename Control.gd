e373baa824ae48cd6df712bb7c71a6c1081533f4
Ryder Fluevog <1248410@edu.burnabyschools.ca>
2024-04-18 11:22:47 -0700

    test

--- Control.gd
+++ Control.gd
@@ -8,12 +8,16 @@ var isUppercase = false
 @onready var WithUppercase = $Button3/WithUppercase
 @onready var WithSpaces = $Button4/WithSpaces
 @onready var itemlist = $Button5/ItemList
+@onready var OneWord = $Button7/OneWord
 var names = ["Dog", "Cat", "Horse"]
 var names2 = ["Big", "Sad", "Happy"]
 var symbols = ["!", "@", "%", "-"]
+var randomName = ["Dog", "Cat", "Horse"]
+var name1 = ""
+var name2 = ""
 
-func _ready(): #Ryder 
-	
+
+func _ready(): #Ryder
 	for i in names:
 		itemlist.add_item(str(i))
 	
@@ -24,27 +28,28 @@ func generateName(): #Written by Owen
 	var name1 = ""
 	var name2 = ""
 	if WithUppercase.button_pressed == true:
-		while not name1.contains(Line.text): #Loops indefinitely if supplied string does not match the list
-			name1 = names[rng.randi_range(0,names.size()-1)]
-			name2 = names2[rng.randi_range(0,names2.size()-1)]
+		
+		name1 = names[rng.randi_range(0,names.size()-1)]
+		name2 = names2[rng.randi_range(0,names2.size()-1)]
 	else:
-		while not name1.contains(Line.text):
-			name1 = names[rng.randi_range(0,names.size()-1)].to_lower()
-			name2 = names2[rng.randi_range(0,names2.size()-1)].to_lower()
+		name1 = names[rng.randi_range(0,names.size()-1)].to_lower()
+		name2 = names2[rng.randi_range(0,names2.size()-1)].to_lower()
 	if WithSpaces.button_pressed:
 		return(str(name2, " ", name1))
 	else:
 		return(str(name2,name1))
+	
+	
 
 func GenerateSymbols():
-		var gensymbols = symbols[rng.randi_range(0,symbols.size()-1)]
-		return(gensymbols)
+	var gensymbols = symbols[rng.randi_range(0,symbols.size()-1)]
+	return(gensymbols)
 
-func generateNumbers(lower,upper): #Written by Ryder 
+func generateNumbers(lower,upper):
 	var number = rng.randi_range(lower,upper)
 	return(number)
 
-func _on_button_6_pressed(): #Written by Ryder
+func _on_button_6_pressed():
 	var concatenatedname
 	print(generateName())
 	if WithNumbers.button_pressed == true:
@@ -53,3 +58,10 @@ func _on_button_6_pressed(): #Written by Ryder
 		print(generateNumbers(10,99))
 	if WithSymbols.button_pressed == true:
 		print(GenerateSymbols())
+		
+
+func _on_button_7_pressed():
+	if OneWord.button_pressed == true:
+		print(str(name1))
+		
+		
